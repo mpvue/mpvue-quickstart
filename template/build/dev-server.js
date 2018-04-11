b18d1f8f2a78082fcd4376fc7085e85afa0c0094
bigMeow <running@vip.163.com>
2018-04-11 17:05:05 +0800

    注释掉未使用变量

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -65,7 +65,7 @@ app.use(require('connect-history-api-fallback')())
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+// var uri = 'http://localhost:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
