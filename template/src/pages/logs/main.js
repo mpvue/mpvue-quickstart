7c6dd7e41b1d6649eff95f036ce25f3ba60aaf57
aorz <rememberwz@gmail.com>
2018-08-10 17:25:51 +0800

    update: mpvue-loader version

--- template/src/pages/logs/main.js
+++ template/src/pages/logs/main.js
@@ -3,9 +3,3 @@ import App from './index'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 const app = new Vue(App){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 app.$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
-export default {
-  config: {
-    navigationBarTitleText: '查看启动日志'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
