d8186b02444499515afe50141a74f6934133b8ed
hucq <chengquan.hu@gmail.com>
2018-11-01 13:27:58 +0800

    update: 增加配置选项，可制定构建平台

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -1,5 +1,6 @@
 require('./check-versions')()
 
+process.env.PLATFORM = process.argv[process.argv.length - 1] || 'wx'
 var config = require('../config')
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
