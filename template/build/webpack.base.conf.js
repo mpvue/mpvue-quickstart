be2af63baec038f7a14d4084737d68d8b77031ff
ShiningDan <zhuce.zyc@gmail.com>
2018-12-01 10:18:06 +0800

    feat: add project.swan.config in dist/swan

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -26,7 +26,7 @@ const appEntry = { app: resolve('./src/main.js') }
 const pagesEntry = getEntry(resolve('./src'), 'pages/**/main.js')
 const entry = Object.assign({}, appEntry, pagesEntry)
 
-module.exports = {
+let baseWebpackConfig = {
   // 如果要自定义生成的 dist 目录里面的文件路径，
   // 可以将 entry 写成 {'toPath': 'fromPath'} 的形式，
   // toPath 为相对于 dist 的路径, 例：index/demo，则生成的文件地址为 dist/index/demo.js
@@ -124,3 +124,19 @@ module.exports = {
     ])
   ]
 }
+
+// 针对百度小程序，由于不支持通过 miniprogramRoot 进行自定义构建完的文件的根路径
+// 所以需要将项目根路径下面的 project.swan.json 拷贝到 dist/swan 下
+// 然后百度开发者工具将 dist/swan 作为项目根目录打开进行调试
+if (process.env.PLATFORM === 'swan') {
+  baseWebpackConfig = merge(baseWebpackConfig, {
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: path.resolve(__dirname, '../project.swan.json'),
+        to: path.resolve(config.build.assetsRoot)
+      }])
+    ]
+  })
+}
+
+module.exports = baseWebpackConfig
